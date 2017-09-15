ce1ff677c5ddf59433d05e1b2b94d14565184200
sushi1998 <sushi1998@users.noreply.github.com>
2017-09-15 10:10:40 -0700

    Update fibonacci_skeleton.sol

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -5,25 +5,25 @@ contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
 		greeter greeting;
-		uint res = 0;
+		uint result = 0;
 		if (position == 0) {
-		res = 0;
+		result = 0;
 		}
 		else if (position == 1) {
 		return 1;
 		}
 		else {
-			uint k1 = 0;
-			uint k2 = 1;
+			uint a = 0;
+			uint b = 1;
 			uint temp = 0;
 			for (uint i = 1; i < position; i++) {
-				temp = k2;
-				k2 = k2 + k1;
-				k1 = temp;
+				temp = b;
+				b = b + a;
+				a = temp;
 			}
-		res = k2;
+		result = b;
 		}
-		return res;
+		return result;
 	}
 	}
 
