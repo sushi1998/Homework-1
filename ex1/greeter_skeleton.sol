8a24f0300404f9d887f7a3c69eb98e4ef8728627
sushi1998 <sushi1998@users.noreply.github.com>
2017-09-15 10:03:55 -0700

    Update greeter_skeleton.sol

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -7,9 +7,11 @@ contract greeter {
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		return greeting;
 	}
 }
